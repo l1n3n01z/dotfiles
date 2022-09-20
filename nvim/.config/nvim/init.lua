82879929e52c00a6035a12052a5b65e4f6794dd3
Nonni <jonthoroddsen@gmail.com>
2022-09-20 11:45:11 +0000

    Plugins setup. Packer installation. Added Comment.nvim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,7 +1,7 @@
 require "user.options"
 require "user.keymaps"
 require "user.system"
---require "user.plugins"
+require "user.plugins"
 --require "user.autocommands"
 --require "user.colorscheme"
 --require "user.cmp"
