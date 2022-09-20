8671186c0ed499fd01abacf6db7638331cefe22c
Nonni <jonthoroddsen@gmail.com>
2022-09-20 11:06:36 +0000

    refactoring os specific functionality to one file

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,34 +1,6 @@
--- NO LONGER NEEDED. neovim supports has('wsl')
---
---local function check_wsl () 
---  if not (vim.fn.has('unix')) then return false end
---  return os.getenv("WSL_INTEROP") ~= nil
---end
---asdka;l;
--- explicit setting the clipboard reduces startup time by 500 ms. 
--- Clip.exe seems to have the least latency
--- If we want better performance we are going to have to write a server queue for the clipboard
-if (vim.fn.has('wsl')) then
-  vim.g.clipboard = {
-    name = "clipfunctions",
-    copy = {
-      ["+"] = "clip.exe",
-      ["*"] = "clip.exe"
-    },
-    paste = {
-      ["+"] = "win32yank.exe -o --lf",
-      ["*"] = "win32yank.exe -o --lf"
-    },
-    cache_enabled = 0
-  }
-  -- let's not swamp the clipboard with single characters
-  vim.keymap.set("n", "x", "\"0x", { silent = true })
-  vim.keymap.set("n", "X", "\"0X", { silent = true })
-end
-
-
 require "user.options"
 require "user.keymaps"
+require "user.system"
 --require "user.plugins"
 --require "user.autocommands"
 --require "user.colorscheme"
