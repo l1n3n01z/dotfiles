b23fbb2f4d4ead2f02f38176070b4edab19ca15a
Nonni <jonthoroddsen@gmail.com>
2022-09-20 13:31:04 +0000

    habamax as default colorscheme

--- nvim/.config/nvim/lua/user/colorscheme.lua
+++ nvim/.config/nvim/lua/user/colorscheme.lua
@@ -1,7 +1,6 @@
 local colorscheme = "darkplus"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
-if not status_ok then
-  vim.notify("colorscheme " .. colorscheme .. " is not available")
-  return
-end
+if (status_ok) then return end
+vim.notify("colorscheme " .. colorscheme .. " is not available. Switching to habamax")
+local status_ok, _ = pcall(vim.cmd, "colorscheme habamax")
