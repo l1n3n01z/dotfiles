aedc00b7eb503978bc9587be5b0e4c4302b0ee0f
Nonni <jonthoroddsen@gmail.com>
2022-10-11 10:47:32 +0000

    Commiting init file to show organization

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,10 +1,18 @@
+-- basic things
 require "user.options"
 require "user.keymaps"
 require "user.commands"
 require "user.system"
+
+-- install plugins 
 require "user.plugins"
+
+
+-- basic things that depend on plugins
 require "user.autocommands"
 require "user.colorscheme"
+
+-- plugin setups
 require "user.cmp"
 require "user.telescope"
 require "user.treesitter"
